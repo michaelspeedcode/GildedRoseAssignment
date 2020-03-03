0e60f143ea64195bda18eda75485bf5aee01c2d8
MSpeed <michaelcspeed+git@gmail.com>
2020-03-03 19:42:55 +0100

    case for nomal items

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,28 +2,46 @@ package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
 
+    private fun updateNormal(item: Item) {
+        item.sellIn -= 1;
+        if (item.quality == 0) return
+
+        item.quality -= 1;
+
+        if (item.sellIn <= 0) {
+            item.quality -= 1;
+        }
+    }
+
     fun updateQuality() {
         for (i in items.indices) {
+
+            when (items[i].name){
+                "normal" -> updateNormal(items[i])
+            }
+
+            return
+
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > 0) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+                        items[i].quality -= 1
                     }
                 }
             } else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                    items[i].quality += 1
 
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality += 1
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality += 1
                             }
                         }
                     }
@@ -39,7 +57,7 @@ class GildedRose(var items: Array<Item>) {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].quality > 0) {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+                                items[i].quality -= 1
                             }
                         }
                     } else {
