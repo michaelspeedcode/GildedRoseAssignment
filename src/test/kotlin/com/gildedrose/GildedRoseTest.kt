084c48f4a3336147bc0453e579bb80beaaeca748
MSpeed <michaelcspeed+git@gmail.com>
2020-03-03 10:42:11 +0100

    added tests to fit requirements

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,19 +1,161 @@
 package com.gildedrose
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    @Test
+    fun foo() {
+        val items = arrayOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
-
+        assertEquals("foo", app.items[0].name)
     }
 
+    @Test
+    fun item_beforeSellByDateTest() {
+        // At the end of each day our system lowers both values for every item
+
+        val initialSellIn = 20
+        val initialQuality = 25
+
+        val items = arrayOf(Item("foo", initialSellIn, initialQuality))
+        val app = GildedRose(items)
+        for (i in 0 until initialSellIn) {
+            app.updateQuality()
+            assertEquals((initialSellIn - 1) - i, app.items[0].sellIn)
+            assertEquals((initialQuality - 1) - i, app.items[0].quality)
+        }
+    }
+
+    @Test
+    fun item_afterSellByDateTest() {
+        // Once the sell by date has passed, Quality degrades twice as fast
+
+        val initialSellIn = 0
+        val initialQuality = 25
+
+        val items = arrayOf(Item("foo", initialSellIn, initialQuality))
+        val app = GildedRose(items)
+        for (i in 1..10) {
+            app.updateQuality()
+            assertEquals(-i, app.items[0].sellIn)
+            assertEquals(initialQuality - 2 * i, app.items[0].quality)
+        }
+    }
+
+    @Test
+    fun agedBrie_beforeSellByDateTest() {
+        //"Aged Brie" actually increases in Quality the older it gets
+
+        val initialSellIn = 10
+        val initialQuality = 15
+
+        val items = arrayOf(Item("Aged Brie", initialSellIn, initialQuality))
+        val app = GildedRose(items)
+        for (i in 1 until initialSellIn) {
+            app.updateQuality()
+            assertEquals(initialSellIn - i, app.items[0].sellIn)
+            assertEquals(50.coerceAtMost(initialQuality + i), app.items[0].quality)
+        }
+    }
+
+    @Test
+    fun agedBrieAfterSellByDateTest() {
+        //"Aged Brie" actually increases in Quality the older it gets
+
+        val initialSellIn = 0
+        val initialQuality = 20
+
+        val items = arrayOf(Item("Aged Brie", initialSellIn
+                , initialQuality))
+        val app = GildedRose(items)
+        for (i in 1..10) {
+            app.updateQuality()
+            assertEquals(-i, app.items[0].sellIn)
+            assertEquals(50.coerceAtMost(initialQuality + 2 * i), app.items[0].quality)
+        }
+    }
+
+    @Test
+    fun sulfurasTest() {
+        // Sulfuras doesn't age
+
+        val initialSellIn = 0
+        val initialQuality = 80
+
+        val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", initialSellIn, initialQuality))
+        val app = GildedRose(items)
+        for (i in 1..30) {
+            app.updateQuality()
+            assertEquals(0, app.items[0].sellIn)
+            assertEquals(initialQuality, app.items[0].quality)
+        }
+    }
+
+    @Test
+    fun backstagePass_AfterSellByDateTest() {
+        //	Quality drops to 0 after the concert
+
+        val initialSellIn = 0
+        val initialQuality = 4
+
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", initialSellIn, initialQuality))
+        val app = GildedRose(items)
+        for (i in 1..30) {
+            app.updateQuality()
+            assertEquals(-i, app.items[0].sellIn)
+            assertEquals(0, app.items[0].quality)
+        }
+    }
+
+    @Test
+    fun backstagePass_Within10DaysTest() {
+        //	Quality increases by 2 when there are 10 days or less
+
+        val initialSellIn = 10
+        val initialQuality = 40
+
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", initialSellIn, initialQuality))
+        val app = GildedRose(items)
+        for (i in 1..10) {
+            app.updateQuality()
+            assertEquals(initialSellIn - i, app.items[0].sellIn)
+            assertEquals(50.coerceAtMost(initialQuality + 2 * i), app.items[0].quality)
+        }
+    }
+
+    @Test
+    fun backstagePassSellWithin5Days() {
+        // ... and by 3 when there are 5 days or less but
+
+        val initialSellIn = 5
+        val initialQuality = 40
+
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", initialSellIn, initialQuality))
+        val app = GildedRose(items)
+        for (i in 1..5) {
+            app.updateQuality()
+            assertEquals(initialSellIn - i, app.items[0].sellIn)
+            assertEquals(50.coerceAtMost(initialQuality + 3 * i), app.items[0].quality)
+        }
+    }
+
+//    @Test
+//    fun conjuredItemsTest() {
+//        // "Conjured" items degrade in Quality twice as fast as normal items
+//
+//        val initialSellIn = 20
+//        val initialQuality = 25
+//
+//        val items = arrayOf(Item("Conjured Mana Cake", initialSellIn, initialQuality))
+//        val app = GildedRose(items)
+//        for (i in 0 until initialSellIn) {
+//            app.updateQuality()
+//            assertEquals((initialSellIn - 1) - i, app.items[0].sellIn)
+//            assertEquals((initialQuality - 2) - i, app.items[0].quality)
+//        }
+//    }
 
 }
-
-
